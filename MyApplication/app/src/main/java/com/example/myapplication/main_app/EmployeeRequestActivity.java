3285750954e7050c20df98fd9bcc09b99e3d15bf
MinkNhat <nquyenminhnhat0503@gmail.com>
2024-11-19 16:23:42 +0700

    add interface, recode request employee

--- MyApplication/app/src/main/java/com/example/myapplication/main_app/EmployeeRequestActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/main_app/EmployeeRequestActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication.main_app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +15,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.Configuration;
 import com.example.myapplication.R;
 import com.example.myapplication.database.AppDatabase;
 import com.example.myapplication.database.entities.Employee;
+import com.example.myapplication.database.entities.Role;
+import com.example.myapplication.database.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRequestActivity extends AppCompatActivity {
 
     ListView mListView;
     Button btnBack;
+    List<Employee> mlist;
+    RequestEmployeeAdapter profileAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +47,97 @@ public class EmployeeRequestActivity extends AppCompatActivity {
         bindingView();
 
         // Lấy list ds đơn cần duyệt
-        List<Employee> mlist = AppDatabase.getInstance(this).employeeDao().getInactiveEmployees();
+        mlist = AppDatabase.getInstance(this).employeeDao().getInactiveEmployees();
+        if(mlist.isEmpty()) {
+            Toast.makeText(this, "Không có đơn yêu cầu nào cần được duyệt!", Toast.LENGTH_SHORT).show();
+        }
 
         // Set adapter
-        RequestEmployeeAdapter profileAdapter = new RequestEmployeeAdapter(EmployeeRequestActivity.this, mlist);
+        profileAdapter = new RequestEmployeeAdapter(EmployeeRequestActivity.this, mlist, new RequestEmployeeAdapter.IClickItemEmployee() {
+            @Override
+            public void approveEmployee(Employee employee) {
+                handleApproveClick(employee);
+            }
+
+            @Override
+            public void disApproveEmployee(Employee employee) {
+                handleDisapproveClick(employee);
+            }
+        });
+
         mListView.setAdapter(profileAdapter);
 
         btnBack.setOnClickListener(v -> {
             finish();
         });
+
+    }
+
+    private void handleApproveClick(Employee employee) {
+        String to = employee.getEmail();
+        String sub = "Đăng ký thông tin thành công!!!";
+
+        String username = Configuration.makeUsername(employee.getFullName(), employee.getPhoneNumber());
+        String password = Configuration.randomString(10);
+
+        String content =
+                "Chúc mừng bạn đã đăng ký thông tin thành công, đây là tài khoản và mật khẩu của bạn. \n" +
+                        "Vui lòng đổi mật khẩu trong lần đăng nhập đầu tiên! \n\n" +
+                        "Tên tài khoản: " + username + "\n" +
+                        "Mật khẩu: " + password + "\n";
+
+        // Gửi mail chứa tài khoản và mật khẩu
+        Configuration.sendMail(this, to, sub, content);
+
+        try {
+            // Lưu user xuống db
+            Role EmployeeRole = AppDatabase.getInstance(this).roleDao().getRoleByName("Employee");
+            // check có role là Employee hay chưa, nếu chưa có thì tạo role
+            if(EmployeeRole == null) {
+                EmployeeRole = new Role("Employee", "Nhân viên quèn");
+                AppDatabase.getInstance(this).roleDao().insert(EmployeeRole);
+                EmployeeRole = AppDatabase.getInstance(this).roleDao().getRoleByName("Employee");
+            }
+
+            // Thêm user vào db
+            User user = new User(username, Configuration.md5(password), Configuration.STRING_TODAY, EmployeeRole.getRoleId());
+            AppDatabase.getInstance(this).userDao().insert(user);
+            user = AppDatabase.getInstance(this).userDao().getUserByUsername(username);
+
+            // Set active và gán user cho employee
+            employee.setActive(1);
+            employee.setUserId(user.getUserId());
+            AppDatabase.getInstance(this).employeeDao().update(employee);
+
+            // Xoá khỏi list chờ duyệt và lưu thay đổi
+            mlist.remove(employee);
+            profileAdapter.notifyDataSetChanged();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleDisapproveClick(Employee employee) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Cảnh báo!");
+        dialog.setMessage("Bạn có chắc chắn muốn từ chối?");
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                mlist.remove(employee);
+                AppDatabase.getInstance(EmployeeRequestActivity.this).employeeDao().delete(employee);
+                profileAdapter.notifyDataSetChanged();
+            }
+        });
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        dialog.create().show();
     }
 
     private void bindingView() {
--- MyApplication/app/src/main/java/com/example/myapplication/main_app/RequestEmployeeAdapter.java
+++ MyApplication/app/src/main/java/com/example/myapplication/main_app/RequestEmployeeAdapter.java
@@ -43,12 +43,20 @@ import javax.mail.internet.MimeMessage;
 public class RequestEmployeeAdapter extends ArrayAdapter<Employee> {
     private Context context;
     private List<Employee> list;
+    private IClickItemEmployee iClickItemEmployee;
 
-    public RequestEmployeeAdapter(@NonNull Context context, List<Employee> list) {
+    public RequestEmployeeAdapter(@NonNull Context context, List<Employee> list, IClickItemEmployee iClickItemEmployee) {
         super(context, 0, list);
         this.context = context;
         this.list = list;
+        this.iClickItemEmployee = iClickItemEmployee;
     }
+
+    public interface IClickItemEmployee {
+        void approveEmployee(Employee employee);
+        void disApproveEmployee(Employee employee);
+    }
+
 
     @NonNull
     @Override
@@ -70,85 +78,16 @@ public class RequestEmployeeAdapter extends ArrayAdapter<Employee> {
         Button btnApprove = convertView.findViewById(R.id.btn_approve);
         Button btnDisapprove = convertView.findViewById(R.id.btn_disapprove);
 
-
-
+        // Xử lý click btn trong item
         btnApprove.setOnClickListener(view -> {
-            handleApproveClick(employee);
+            iClickItemEmployee.approveEmployee(employee);
         });
 
         btnDisapprove.setOnClickListener(view -> {
-            handleDisapproveClick(employee);
+            iClickItemEmployee.disApproveEmployee(employee);
         });
 
         return convertView;
     }
-
-    private void handleApproveClick(Employee employee) {
-        String to = employee.getEmail();
-        String sub = "Đăng ký thông tin thành công!!!";
-
-        String username = Configuration.makeUsername(employee.getFullName(), employee.getPhoneNumber());
-        String password = Configuration.randomString(10);
-
-        String content =
-                "Chúc mừng bạn đã đăng ký thông tin thành công, đây là tài khoản và mật khẩu của bạn. \n" +
-                "Vui lòng đổi mật khẩu trong lần đăng nhập đầu tiên! \n\n" +
-                "Tên tài khoản: " + username + "\n" +
-                "Mật khẩu: " + password + "\n";
-
-
-        Configuration.sendMail(context, to, sub, content);
-
-        try {
-            // Lưu user xuống db
-            Role EmployeeRole = AppDatabase.getInstance(this.getContext()).roleDao().getRoleByName("Employee");
-            if(EmployeeRole == null) {
-                EmployeeRole = new Role("Employee", "Nhân viên quèn");
-                AppDatabase.getInstance(this.getContext()).roleDao().insert(EmployeeRole);
-                EmployeeRole = AppDatabase.getInstance(this.getContext()).roleDao().getRoleByName("Employee");
-            }
-
-            User user = new User(username, Configuration.md5(password), Configuration.STRING_TODAY, EmployeeRole.getRoleId());
-            AppDatabase.getInstance(this.getContext()).userDao().insert(user);
-            user = AppDatabase.getInstance(this.getContext()).userDao().getUserByUsername(username);
-
-            employee.setActive(1);
-            employee.setUserId(user.getUserId());
-            AppDatabase.getInstance(this.getContext()).employeeDao().update(employee);
-            list.remove(employee);
-            this.notifyDataSetChanged();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void handleDisapproveClick(Employee employee) {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this.getContext());
-        dialog.setTitle("Cảnh báo!");
-        dialog.setMessage("Bạn có chắc chắn muốn từ chối?");
-        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                removeEmployee(employee);
-            }
-        });
-        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-
-        dialog.create().show();
-    }
-
-    private void removeEmployee(Employee employee) {
-        list.remove(employee);
-        AppDatabase.getInstance(this.getContext()).employeeDao().delete(employee);
-        this.notifyDataSetChanged();
-    }
-
 
 }
