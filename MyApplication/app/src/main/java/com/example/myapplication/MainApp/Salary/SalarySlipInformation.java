05dadc3ee1e179c966ab95212f1493be7b18a2a5
Nghia Le <nghialntilactuca@gmail.com>
2024-12-11 13:54:30 +0700

    add enum tax

--- MyApplication/app/src/main/java/com/example/myapplication/MainApp/Salary/SalarySlipInformation.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainApp/Salary/SalarySlipInformation.java
@@ -20,10 +20,14 @@ import com.example.myapplication.database.AppDatabase;
 import com.example.myapplication.database.entities.Department;
 import com.example.myapplication.database.entities.Employee;
 import com.example.myapplication.database.entities.Employee_RewardDiscipline;
+import com.example.myapplication.database.entities.Employee_Session;
 import com.example.myapplication.database.entities.Position;
 import com.example.myapplication.database.entities.Salary;
+import com.example.myapplication.database.entities.Session;
+import com.example.myapplication.database.entities.Timekeeping;
 import com.example.myapplication.database.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SalarySlipInformation extends AppCompatActivity {
@@ -31,6 +35,7 @@ public class SalarySlipInformation extends AppCompatActivity {
     private int userId;
     private int month;
     private int year;
+
     private ImageView imgEmployee;
     private Button btnBack;
     private TextView tvEmployeeName;
@@ -38,6 +43,7 @@ public class SalarySlipInformation extends AppCompatActivity {
     private TextView tvPosition;
     private TextView tvPosition2;
     private TextView tvDepartment;
+    private TextView tvOvertime;
     private TextView tvBaseSalary;
     private TextView tvAllowance;
     private TextView tvRewardDiscipline;
@@ -45,8 +51,8 @@ public class SalarySlipInformation extends AppCompatActivity {
     private TextView tvTotalSalary;
     private TextView tvReceiveSalary;
     private TextView tvReceiveSalary2;
-    private List<Employee_RewardDiscipline> employeeRewardDisciplines;
 
+    private List<Employee_RewardDiscipline> employeeRewardDisciplines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,6 +127,7 @@ public class SalarySlipInformation extends AppCompatActivity {
         String fullName = (employee.getFullName() != null) ? employee.getFullName() : "Không có tên";
         String positionName = (employee.getPositionId() != null) ? getPositionName(employee.getPositionId()) : "Không có chức vụ";
         String departmentName = (employee.getDepartmentId() != null) ? getDepartmentName(employee.getDepartmentId()) : "Không có phòng ban";
+        Float overtime = getEmployeeOvertime(employee.getEmployeeId(), month, year);
         Float baseSalary = (employee.getSalaryId() != null) ? getBaseSalary(employee.getSalaryId()) : 0;
         Float allowanceSalary = (employee.getSalaryId() != null) ? getAllowanceSalary(employee.getSalaryId()) : 0;
         Float rewardDisciplineMoney = (getRewardDisciplineMoney(employee.getEmployeeId(), month, year));
@@ -133,6 +140,7 @@ public class SalarySlipInformation extends AppCompatActivity {
         tvPosition.setText(positionName);
         tvPosition2.setText(positionName);
         tvDepartment.setText(departmentName);
+        tvOvertime.setText(String.format("%.1f giờ", overtime));
         tvBaseSalary.setText(String.format("%,.0f VND", baseSalary));
         tvAllowance.setText(String.format("%,.0f VND", allowanceSalary));
         tvRewardDiscipline.setText(String.format("%,.0f VND", rewardDisciplineMoney));
@@ -187,27 +195,40 @@ public class SalarySlipInformation extends AppCompatActivity {
         return totalReward;
     }
 
-    // Tính thuế thu nhập
-    public static double calculateTax(double tntt) {
-        double tax = 0;
+    private float getEmployeeOvertime(int employeeId, int month, int year) {
+        float totalOvertime = 0;
 
-        if (tntt <= 5_000_000) {
-            tax = tntt * 0.05;
-        } else if (tntt <= 10_000_000) {
-            tax = tntt * 0.10 - 250_000;
-        } else if (tntt <= 18_000_000) {
-            tax = tntt * 0.15 - 750_000;
-        } else if (tntt <= 32_000_000) {
-            tax = tntt * 0.20 - 1_650_000;
-        } else if (tntt <= 52_000_000) {
-            tax = tntt * 0.25 - 3_250_000;
-        } else if (tntt <= 80_000_000) {
-            tax = tntt * 0.30 - 5_850_000;
-        } else {
-            tax = tntt * 0.35 - 9_850_000;
+        try {
+            List<Session> sessions = new ArrayList<>();
+            List<Employee_Session> employeeSessions = AppDatabase.getInstance(this)
+                    .employeeSessionDao().getSessionByEmployeeId(employeeId);
+
+            for (Employee_Session employeeSession : employeeSessions) {
+                Session session = AppDatabase.getInstance(this).sessionDao()
+                        .getSessionById(employeeSession.getSessionID());
+
+                if (session.getMonth() == month && session.getYear() == year) {
+                    sessions.add(session);
+                }
+            }
+
+            for (Session session : sessions) {
+                List<Timekeeping> timekeepings = AppDatabase.getInstance(this)
+                        .timekeepingDao().getTimekeepingBySessionId(session.getSessionId());
+
+                for (Timekeeping timekeeping : timekeepings) {
+                    totalOvertime += timekeeping.getOvertime();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Lỗi tính overtime~", Toast.LENGTH_SHORT).show();
         }
 
-        return tax;
+        return totalOvertime / 60;
+    }
+
+    public static double calculateTax(double tntt) {
+        return TaxBracket.getTax(tntt);
     }
 
     private void initUI() {
@@ -217,6 +238,7 @@ public class SalarySlipInformation extends AppCompatActivity {
         tvPosition = findViewById(R.id.tv_position);
         tvPosition2 = findViewById(R.id.tv_position2);
         tvDepartment = findViewById(R.id.tv_department);
+        tvOvertime = findViewById(R.id.tv_overtime);
         tvBaseSalary = findViewById(R.id.tv_base_salary);
         tvAllowance = findViewById(R.id.tv_allowance);
         tvRewardDiscipline = findViewById(R.id.tv_employee_reward_discipline);
